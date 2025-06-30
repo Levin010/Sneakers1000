e28c695ed613a5e154a7c0d32310c350304955ad
Levin010 <levinomwenga10@gmail.com>
2025-06-30 11:52:26 +0300

    Add Sneaker.vue, loading.js and SneakerBox

---
+++ frontend/src/views/Sneaker.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="page-product">
+        <div class="columns is-multiline">
+            <div class="column is-9">
+                <figure class="image mb-6">
+                    <img v-bind:src="sneaker.get_image">
+                </figure>
+
+                <h1 class="title">{{ sneaker.name }}</h1>
+
+                <p>{{ sneaker.description }}</p>
+            </div>
+
+            <div class="column is-3">
+                <h2 class="subtitle mb-3">Product Details</h2>
+
+                <p><strong>Price: </strong>KES{{ sneaker.price }}</p>
+
+                <div class="field has-addons mt-6">
+                    <div class="control">
+                        <input type="number" class="input" min="1" v-model="quantity">
+                    </div>
+
+                    <div class="control">
+                        <a class="button is-dark" @click="addToCart()">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { useLoadingStore } from '@/stores/loading'
+
+export default {
+  name: 'Sneaker',
+  data() {
+    return {
+      sneaker: {},
+      quantity: 1
+    }
+  },
+  mounted() {
+    this.getSneaker()
+  },
+  methods: {
+    async getSneaker() {
+      const loading = useLoadingStore()
+      loading.setIsLoading(true)
+
+      const category_slug = this.$route.params.category_slug
+      const sneaker_slug = this.$route.params.sneaker_slug
+
+      try {
+        const response = await axios.get(`/api/v1/sneakers/${category_slug}/${sneaker_slug}`)
+        this.sneaker = response.data
+        document.title = this.sneaker.name + ' | Sneakers1000'
+      } catch (error) {
+        console.log(error)
+      }
+
+      loading.setIsLoading(false)
+    }
+  }
+}
+</script>
--- sneakers_project/sneakers/views.py
+++ sneakers_project/sneakers/views.py
@@ -1,5 +1,9 @@
+from django.db.models import Q
+from django.http import Http404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 
 from .models import Sneaker, Category
 from .serializers import SneakerSerializer, CategorySerializer
@@ -9,4 +13,19 @@ class LatestSneakersList(APIView):
     def get(self, request, format=None):
         sneakers = Sneaker.objects.all()[0:4]
         serializer = SneakerSerializer(sneakers, many=True)
+        return Response(serializer.data)
+
+
+class SneakerDetail(APIView):
+    def get_object(self, category_slug, sneaker_slug):
+        try:
+            return Sneaker.objects.filter(category__slug=category_slug).get(
+                slug=sneaker_slug
+            )
+        except Sneaker.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, sneaker_slug, format=None):
+        sneaker = self.get_object(category_slug, sneaker_slug)
+        serializer = SneakerSerializer(sneaker)
         return Response(serializer.data)
